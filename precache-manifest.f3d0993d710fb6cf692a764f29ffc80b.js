1491830c2fad663005e737fedd382534a1a404fc
AKASH <106383491+lazylad2507@users.noreply.github.com>
2022-12-21 13:28:16 +0530

    Updates

---
+++ precache-manifest.f3d0993d710fb6cf692a764f29ffc80b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "17332e1e59eab41c646a",
+    "url": "/TxtAnalyzer-react/static/css/main.fe5a3cad.chunk.css"
+  },
+  {
+    "revision": "17332e1e59eab41c646a",
+    "url": "/TxtAnalyzer-react/static/js/main.17332e1e.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/TxtAnalyzer-react/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/TxtAnalyzer-react/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "fa1000bcbc8b0061beb5",
+    "url": "/TxtAnalyzer-react/static/js/runtime~main.fa1000bc.js"
+  },
+  {
+    "revision": "c21b8465624614aa13d2d71122391625",
+    "url": "/TxtAnalyzer-react/index.html"
+  }
+];
